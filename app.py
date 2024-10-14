ca4a987abae72ace5d3c32456784490a2d50758f
Bryan Olandres <ozoneblacklight@outlook.com>
2024-10-14 17:35:01 +0800

    removed old route

--- app.py
+++ app.py
@@ -65,7 +65,6 @@ actions = [
 #
 def send_msg_to_slack(action: str, user_id: str, channel_id: str, text: str):
     '''Respond back to Slack on the same channel where the user sent the command'''
-
     match action:
         case 'clock-in':
             response_text = f'<@{user_id}> has *clocked in* :clock1:'
@@ -84,30 +83,6 @@ def send_msg_to_slack(action: str, user_id: str, channel_id: str, text: str):
         case _:
             response_text = f'<@{user_id}> used an unknown command: *{action}*'
 
-    blocks = [{
-        'type': 'section',
-        'text': {
-            'type': 'mrkdwn',
-            'text': response_text
-        }
-    }]
-
-    if action == 'clock-in':
-        blocks.append({
-            "type": "actions",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                    "type": "plain_text",
-                                    "text": "Break 15 mins."
-                            },
-                            "value": "break-15",
-                            "action_id": "break-15"
-                        }
-                    ]
-        })
-
     # append user note at the end of response_text is user added any
     # underscore to italize
     if text:
@@ -116,7 +91,13 @@ def send_msg_to_slack(action: str, user_id: str, channel_id: str, text: str):
     try:
         response = client.chat_postMessage(
             channel=channel_id,
-            blocks=blocks
+            blocks=[{
+                'type': 'section',
+                'text': {
+                    'type': 'mrkdwn',
+                    'text': response_text
+                }
+            }]
         )
         return response
     except SlackApiError as e:
@@ -133,93 +114,6 @@ def convert_timestamp(timestamp: str):
         tz=philippine_tz
     )
     return str(philippine_time.strftime('%Y-%m-%d %H:%M:%S'))
-
-
-# main route to be used by slack
-@app.route('/time/<action>', methods=['POST'])
-def time(action: str):
-    '''Read data sent from Slack then push to db'''
-
-    if action not in actions:
-        return {
-            'status': 'failed',
-            'msg': f"invalid command: '{action}'"
-        }, 400
-
-    # check if timestamp header exists
-    if 'x-slack-request-timestamp' in request.headers:
-        slack_timestamp = request.headers['x-slack-request-timestamp']
-    else:
-        logging.error('Timestamp missing from Slack')
-        return {
-            'status': 'failed',
-            'msg': 'Timestamp missing from Slack http header'
-        }, 400
-
-    # return 400 if any of the form items are missing from Slack's payload
-    for item in slack_form_items:
-        if item not in request.form:
-            err_msg = f'{item} missing from Slack http body'
-            logging.error(err_msg)
-            return {
-                'status': 'failed',
-                'msg': err_msg
-            }, 400
-
-    # try parse timestamp
-    # this will probably never throw but just in case Slack messes up
-    try:
-        int(slack_timestamp)
-    except ValueError as e:
-        logging.error(e)
-        return {
-            'status': 'failed',
-            'msg': 'Unable to parse timestamp from Slack, invalid value'
-        }, 400
-
-    try:
-        (
-            supabase.table('Slack Timestamp')
-            .insert(
-                {
-                    'x-slack-request-timestamp': slack_timestamp,
-                    'token': request.form['token'],
-                    'team_id': request.form['team_id'],
-                    'team_domain': request.form['team_domain'],
-                    'channel_id': request.form['channel_id'],
-                    'channel_name': request.form['channel_name'],
-                    'user_id': request.form['user_id'],
-                    'user_name': request.form['user_name'],
-                    'command': request.form['command'],
-                    'text': request.form['text'],
-                    'api_app_id': request.form['api_app_id'],
-                    'is_enterprise_install': request.form['is_enterprise_install'],
-                    'response_url': request.form['response_url'],
-                    'trigger_id': request.form['trigger_id'],
-                    'timestamp': convert_timestamp(slack_timestamp)
-                }
-            ).execute()
-        )
-
-        # Ensure that the app is added in the Slack channel's integration tab for it to work!
-        # only run slack call on prod
-        if app_mode == 'prod':
-            slack_response = send_msg_to_slack(
-                action=action,
-                user_id=request.form['user_id'],
-                channel_id=request.form['channel_id'],
-                text=request.form['text']
-            )
-            logging.info(slack_response)
-
-        return '', 200
-
-    except Exception as e:
-        logging.exception(e)
-        return {
-            'status': 'failed',
-            'msg': 'Failed to save timestamp to database! Please contact Client Solutions'
-        }, 500
 
 
 # handler for app buttons interactions
@@ -271,15 +165,22 @@ def interactions():
                 'msg': 'Failed to save timestamp to database! Please contact Client Solutions'
             }, 500
 
-        # only run slack call on prod
-        if app_mode == 'prod':
-            slack_response = send_msg_to_slack(
-                action=payload['state']['values']['BfE1N']['select-action']['selected_option']['value'],
-                user_id=payload['user']['id'],
-                channel_id=payload['channel']['id'],
-                text=''
-            )
-            logging.info(slack_response)
+        try:
+            # only run slack call on prod
+            if app_mode == 'prod':
+                slack_response = send_msg_to_slack(
+                    action=payload['state']['values']['BfE1N']['select-action']['selected_option']['value'],
+                    user_id=payload['user']['id'],
+                    channel_id=payload['channel']['id'],
+                    text=''
+                )
+                logging.info(slack_response)
+        except Exception as e:
+            logging.exception(e)
+            return {
+                'status': 'failed',
+                'msg': 'Failed to call Slack API'
+            }, 500
 
         return '', 200
 
