c1b372681227b1ae48307e989405de74d2078b25
Bryan Olandres <ozoneblacklight@outlook.com>
2024-10-16 18:36:43 +0800

    add more tests

--- app.py
+++ app.py
@@ -288,18 +288,29 @@ def services():
 
 #
 class UnitTests(unittest.TestCase):
-    '''Just some basic unit tests'''
+    '''Unit tests for the helper functions'''
 
     def test_build_response(self):
+        '''Tests the "build_response" function'''
+        msg: str = 'this is a test string'
         self.assertEqual(
-            build_response(True, 'Test'),
-            {'status': 'success', 'msg': 'Test'}
+            build_response(True, msg),
+            {'status': 'success', 'msg': msg}
+        )
+        self.assertEqual(
+            build_response(False, msg),
+            {'status': 'failed', 'msg': msg}
         )
 
     def test_timestamp_conversion(self):
+        '''Tests the "convert_timestamp" function'''
         self.assertEqual(
             convert_timestamp('1727689594'),
             '2024-09-30 17:46:34'
+        )
+        self.assertEqual(
+            convert_timestamp('1727689723'),
+            '2024-09-30 17:48:43'
         )
 
 
